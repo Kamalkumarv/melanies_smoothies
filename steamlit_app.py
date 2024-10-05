c8278632ccf87fa396877284966378861aa8124c
Kamalkumarv <kamal.varadarajulu@medicalsolutions.com>
2024-10-05 11:31:37 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
-import streamlit as st
-from snowflake.snowpark.context import get_active_session
+import streamlit as st 
 from snowflake.snowpark.functions import col
 import pandas as pd
 
@@ -15,7 +14,8 @@ name_on_order = st.text_input("Name on Smoothie:","");
 
 
 # Get the current credentials
-session = get_active_session()
+ctx = st.connection("snowflake")
+session = ctx.session();
 #option = st.selectbox("What is your favorite fruit?",session.table("smoothies.public.fruit_options").columns[1])
 #st.write("You have selected: ", option);
  
